c56126d6a78454632bdb5ebc774a011f8e884668
subhankar.bibhu123@gmail.com <subhankar.bibhu123@gmail.com>
2026-03-02 11:26:53 +0530

    Updates

--- _next/static/GvOxok6pNr_x60ZVLdHT1/_buildManifest.js
+++ _next/static/GvOxok6pNr_x60ZVLdHT1/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/creations//_next/:path+",
+        "destination": "/creations/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
